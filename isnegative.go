be6db9a4f9c66d79c922d75eadafd352e62bdeb7
ymakovet <33450994+ymakovet@users.noreply.github.com>
2019-10-16 12:18:10 +0600

    Update isnegative.go
    fix

--- isnegative.go
+++ isnegative.go
@@ -4,8 +4,10 @@ import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
 	if nb > 0 {
-		z01.PrintRune("T\n")
+		z01.PrintRune('T')
+		z01.PrintRune(10)
 	} else {
-		z01.PrintRune("F\n")
+		z01.PrintRune('F')
+		z01.PrintRune(10)
 	}
 }
