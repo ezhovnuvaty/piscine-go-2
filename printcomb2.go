a1efb415647f3e37bf477e83d5c6065ff8890082
ymakovet <33450994+ymakovet@users.noreply.github.com>
2019-10-16 14:38:24 +0600

    Update printcomb2.go
    done

--- printcomb2.go
+++ printcomb2.go
@@ -3,20 +3,23 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for a := '0'; a <= '9'; a++ {
-		for b := '0'; b <= '9'; b++ {
-			if a == '9' && b == '9' {
+	for a1 := '0'; a1 <= '9'; a1++ {
+		for b1 := '0'; b1 <= '9'; b1++ {
+			if a1 == '9' && b1 == '9' {
 				z01.PrintRune(10)
 				return
 			}
-			for c := '0'; c <= '9'; c++ {
-				for d := '1'; d <= '9'; d++ {
-					z01.PrintRune(a)
-					z01.PrintRune(b)
+			for a2 := '0'; a2 <= '9'; a2++ {
+				for b2 := '0'; b2 <= '9'; b2++ {
+					if a2 < a1 || (a2 == a1 && b2 <= b1) {
+						continue
+					}
+					z01.PrintRune(a1)
+					z01.PrintRune(b1)
 					z01.PrintRune(' ')
-					z01.PrintRune(c)
-					z01.PrintRune(d)
-					if a != '9' || b != '8' || c != '9' || d != '9' {
+					z01.PrintRune(a2)
+					z01.PrintRune(b2)
+					if a1 != '9' || b1 != '8' || a2 != '9' || b2 != '9' {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
